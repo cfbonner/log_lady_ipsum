6a0f59a9f235d17de405aa5b8b9de3a2d38a42ef
cfbonner <cfbonner@gmail.com>
2020-06-15 20:20:19 +0100

    Endpoints for fetching words, sentences, and paragraphs

--- lib/log_lady/lorem.ex
+++ lib/log_lady/lorem.ex
@@ -8,4 +8,25 @@ defmodule LogLady.Lorem do
   def get_text do
     Text.all
   end
+
+  def get_words(count) do
+    Text.all
+    |> String.split()
+    |> Enum.take(count)
+    |> Enum.join(" ")
+  end
+
+  def get_sentences(count) do
+    get_text
+    |> String.split(".")
+    |> Enum.take(count)
+    |> Enum.join(".")
+  end
+
+  def get_paragraphs(count) do
+    get_text
+    |> String.split("\n\n")
+    |> Enum.take(count)
+    |> Enum.join("\n")
+  end
 end
--- lib/log_lady_web/controllers/lorem_controller.ex
+++ lib/log_lady_web/controllers/lorem_controller.ex
@@ -3,6 +3,24 @@ defmodule LogLadyWeb.LoremController do
 
   alias LogLady.Lorem
 
+  def index(conn, %{"paragraphs" => paragraphs}) do
+    {count, _} = Integer.parse(paragraphs)
+    text = Lorem.get_paragraphs(count)
+    render(conn, "index.json", text: text)
+  end
+
+  def index(conn, %{"sentences" => sentences}) do
+    {count, _} = Integer.parse(sentences)
+    text = Lorem.get_sentences(count)
+    render(conn, "index.json", text: text)
+  end
+
+  def index(conn, %{"words" => words}) do
+    {count, _} = Integer.parse(words)
+    text = Lorem.get_words(count)
+    render(conn, "index.json", text: text)
+  end
+
   def index(conn, _params) do
     render(conn, "index.json", text: Lorem.get_text)
   end
