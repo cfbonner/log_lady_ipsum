4e5e7d79e042e04f490e0a8fc27a254d2e95018c
cfbonner <cfbonner@gmail.com>
2021-08-25 12:13:36 +0100

    Update cors config

--- lib/log_lady_web/cors.ex
+++ lib/log_lady_web/cors.ex
@@ -1,8 +1,7 @@
 defmodule LogLadyWeb.CORS do
   use Corsica.Router,
-    origins: ["http://localhost", ~r{^http?://(.*\.)?fakeshrimp\.com$}],
     allow_credentials: true,
     max_age: 600
 
-  resource "/api/*", origins: ["http://localhost", ~r{^http?://(.*\.)?fakeshrimp\.com$}]
+  resource "/api/*", origins: ["http://localhost:3000", ~r{^http?://(.*\.)?fakeshrimp\.com$}]
 end
